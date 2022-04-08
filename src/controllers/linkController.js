cb796f158819110ac1df52e0a492a89c3adc0a37
pkrlfamous <pkrlfamous23@gmail.com>
2022-04-08 18:49:18 +0545

    link crud done, validation and error handling remained

--- src/controllers/linkController.js
+++ src/controllers/linkController.js
@@ -3,9 +3,36 @@ const {Link} = require('../models');
 const _ = require('lodash');
 
 module.exports = {
+    getALink: async(req, res, next)=>{
+        const link = await Link.findById(req.params);
+        if(!link) return res.status(404).send('link not found');
+        res.status(200).send(link);
+    },
+    getLinks: async(req, res, next)=>{
+        const links = await Link.find({},{__v:0}).sort('title');
+        res.send(links)
+    },
     createLink: async(req, res, next) =>{
         let link = new Link(_.pick(req.body, ['name', 'url', 'parentLink', 'createdBy', 'tag']));
         await link.save();
         res.send(link);
-    }
+    },
+    updateLink: async(req, res, next)=>{
+
+        const link = await Link.findByIdAndUpdate(req.params.id,
+            {
+                title:req.body.title,
+                url:req.body.url,
+                parentLink:req.body.parentLink,
+                createdBy:req.body.createdBy,
+                description:req.body.description,
+                tag:req.body.tag
+            }, {new:true});
+        if(!link) return res.status(404).send('link not found');
+    },
+    deleteLink: async(req, res, next)=>{
+        const link = await Link.findByIdAndRemove(req.params.id);
+        if(!link) return res.status(404).send('link not found');
+        res.send(link);
+    },
 }
--- src/routes/link.js
+++ src/routes/link.js
@@ -2,7 +2,9 @@ const express = require('express');
 const linkController = require('../controllers/linkController');
 
 const router = express.Router();
-
+router.get('/', linkController.getALink);
+router.get('/:id', linkController.getLinks);
 router.post('/createlink', linkController.createLink);
-
+router.put('/:id', linkController.updateLink);
+router.delete('/:id', linkController.deleteLink);
 module.exports = router;
