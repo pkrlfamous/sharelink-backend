49785fabffcba332deeb05b034bd772c05c655f3
pkrlfamous <pkrlfamous23@gmail.com>
2022-04-10 19:46:39 +0545

    handling unhandled routes

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ require('./src/config/dbConfig')();
 
 app.use('/api/v1', require('./src/routes'));
 
+
+app.use('*', (req, res, next)=>{
+    res.status(404).json({
+        'status':'fail',
+        'message':`Can't find ${req.originalUrl} on this server`
+    });
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`listening to ${port}`);
