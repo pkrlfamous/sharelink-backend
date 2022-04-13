80fda913107efda992e00f7b5a882a8279fb8eb7
pkrlfamous <pkrlfamous23@gmail.com>
2022-04-13 14:33:07 +0545

    error handling middleware in progress

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const res = require('express/lib/response');
 const app = express();
 
+const CustomError = require('./src/helpers/errorHelper');
+const globalErrorHandler = require('./src/controllers/errorController');
 app.use(express.json());
 
 require('./src/config/dbConfig')();
@@ -10,27 +12,13 @@ app.use('/api/v1', require('./src/routes'));
 
 
 app.all('*', (req, res, next)=>{
-    // res.status(404).json({
-    //     'status':'fail',
-    //     'message':`Can't find ${req.originalUrl} on this server`
-    // });
-    const err = new Error(`Can't find ${req.originalUrl} on this server`);
-    err.status = 'fail';
-    err.statusCode = 404;
-
-    next(err);
+    next(new CustomError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
 
-app.use((err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
+app.use(globalErrorHandler);
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    });
-});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`listening to ${port}`);
