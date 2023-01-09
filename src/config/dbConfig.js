f5cd682ce28100c2f20798213282535186b5c50e
pkrlfamous <pkrlfamous23@gmail.com>
2023-01-09 16:13:55 +0545

    added digital ocean's database connection string

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,10 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-module.exports = function(){
-    mongoose.connect('mongodb://0.0.0.0/sharelink',{
+module.exports = function () {
+  mongoose
+    .connect(
+      "mongodb+srv://doadmin:GW062jv75qr41Ma9@db-mongodb-blr1-68121-4775f21d.mongo.ondigitalocean.com/admin?tls=true&authSource=admin",
+      {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-    })
-    .then( () => console.log('connected to the database'))
-    .catch(err => console.error('could not connect to db..',err));
-}
+      }
+    )
+    .then(() => console.log("connected to the database"))
+    .catch((err) => console.error("could not connect to db..", err));
+};
