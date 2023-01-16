0fdfeff25df3a791b0353f92b6cef2ae90ef25fb
pkrlfamous <pkrlfamous23@gmail.com>
2023-01-16 11:13:59 +0545

    updated database config.js

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,14 +1,19 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const mongoose = require("mongoose");
 
 module.exports = function () {
-  mongoose
-    .connect(
-      "mongodb+srv://doadmin:GW062jv75qr41Ma9@db-mongodb-blr1-68121-4775f21d.mongo.ondigitalocean.com/admin?tls=true&authSource=admin",
-      {
+  try {
+    const url = process.env.MONGO_URI_ONLINE;
+    mongoose
+      .connect(url, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-      }
-    )
-    .then(() => console.log("connected to the database"))
-    .catch((err) => console.error("could not connect to db..", err));
+      })
+      .then(() => console.log("connected to the database"))
+      .catch((err) => console.error("could not connect to db..", err));
+  } catch (err) {
+    console.log(err);
+  }
 };
